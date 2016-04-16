20fe4f6a3da7a3e6c4dd15360d83e991879876b2
Kasper Lind Sørensen <kasper.lind.sorensen@gmail.com>
2016-04-16 11:26:09 +0200

    Added script for shapeshifting.

---
+++ Assets/Character.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour
+{
+    public GameObject[] children;
+    private int childIndex = 0;
+	
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift))
+        {
+            GameObject previousChild = children[childIndex];
+            Vector3 newPosition = previousChild.transform.position;
+            previousChild.SetActive(false);
+            childIndex = ++childIndex % children.Length;
+            GameObject currentChild = children[childIndex];
+            currentChild.transform.position = newPosition;
+            currentChild.GetComponent<Rigidbody>().velocity = previousChild.GetComponent<Rigidbody>().velocity;
+            currentChild.SetActive(true);
+        }
+    }
+}
