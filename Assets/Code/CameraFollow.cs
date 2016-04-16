f396fd09357292e9b37d1f227c08c375432ad5e9
Kasper Lind Sørensen <kasper.lind.sorensen@gmail.com>
2016-04-16 16:58:17 +0200

    modified CameraFollow script to catch up only when behind rather than matching the y coordinate of the target.

--- Assets/Code/CameraFollow.cs
+++ Assets/Code/CameraFollow.cs
@@ -3,11 +3,21 @@ using System.Collections;
 
 public class CameraFollow : MonoBehaviour {
 
-    void Update() {
+    public float maxDistanceToPlayer = 1f;
+    private float currentYVelocity = 0f;
+
+    void FixedUpdate() {
         float x = transform.position.x;
         float y = Character.currentChild.transform.position.y;
         float z = transform.position.z;
 
-        transform.position = new Vector3(x, y, z);
+        if (transform.position.y > y + maxDistanceToPlayer)
+        {
+            // Camera is falling behind, time to catch up
+            float delta = y - transform.position.y;
+            float target = transform.position.y + delta;
+            Vector3 newPosition = new Vector3(x, Mathf.SmoothDamp(transform.position.y, target, ref currentYVelocity, 0.05f), z);
+            transform.position = newPosition;
+        }
     }
 }
