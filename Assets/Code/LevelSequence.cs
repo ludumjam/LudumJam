a8c617ddc70e1c7214aee9545cf2a83c50ae7df1
magnuschatt <magnus.chatt@gmail.com>
2016-04-16 15:30:52 +0200

    awdaw

--- Assets/Code/LevelSequence.cs
+++ Assets/Code/LevelSequence.cs
@@ -29,8 +29,6 @@ public class LevelSequence : MonoBehaviour {
                 SpawnObstacle(obstacleGO);
             }
         }
-
-        Debug.Log("tick");
     }
 
     private void SpawnObstacle(GameObject obstacle) {
