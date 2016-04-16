3fe116774591e70732643620677bbcdfd2c0cb9f
Kasper Lind Sørensen <kasper.lind.sorensen@gmail.com>
2016-04-16 18:51:07 +0200

    Added UIController which shows / hides the GameOver panel and retry button now reloads scene.

---
+++ Assets/Code/UIController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class UIController : MonoBehaviour
+{
+
+    public GameObject gameOverPanel;
+    public Button retryButton;
+
+    // Use this for initialization
+    void Start()
+    {
+        gameOverPanel.SetActive(false);
+        CameraFollow.OnPlayerWentOutsideScreen += HandleOnDeathEvent;
+        retryButton.onClick.AddListener(HandleRetryButtonOnClick);
+    }
+
+    void HandleOnDeathEvent()
+    {
+        gameOverPanel.SetActive(true);
+    }
+
+    void HandleRetryButtonOnClick()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    void OnDestroy()
+    {
+        CameraFollow.OnPlayerWentOutsideScreen -= HandleOnDeathEvent;
+    }
+}
