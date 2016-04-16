8c807e40a58e0be6d3aceea50d7c2a16ed376e51
Kasper Lind Sørensen <kasper.lind.sorensen@gmail.com>
2016-04-16 10:42:43 +0200

    Added simple character movement script and character prefab.

---
+++ Assets/Character.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Assertions;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Character : MonoBehaviour {
+
+	public float forceModifier = 1f;
+	private new Rigidbody rigidbody;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rigidbody.AddForce (Vector3.right * Input.GetAxis ("Horizontal") * forceModifier, ForceMode.Impulse);
+	}
+}
