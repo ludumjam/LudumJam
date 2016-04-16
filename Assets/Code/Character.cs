791300384e078637e0564d6fc4954ef175b5c9e9
Kasper Lind Sørensen <kasper.lind.sorensen@gmail.com>
2016-04-16 12:19:06 +0200

    Shapeshifting now supports two directions.

--- Assets/Code/Character.cs
+++ Assets/Code/Character.cs
@@ -7,6 +7,8 @@ public class Character : MonoBehaviour
     private int childIndex = 0;
     private GameObject currentChild;
 
+    private enum Shift {Next, Previous};
+
     void Start()
     {
         currentChild = children[0];
@@ -16,16 +18,44 @@ public class Character : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift))
+        if (Input.GetKeyDown(KeyCode.LeftShift))
         {
-            GameObject previousChild = children[childIndex];
-            Vector3 newPosition = previousChild.transform.position;
-            previousChild.SetActive(false);
-            childIndex = ++childIndex % children.Length;
-            currentChild = children[childIndex];
-            currentChild.transform.position = newPosition;
-            currentChild.GetComponent<Rigidbody>().velocity = previousChild.GetComponent<Rigidbody>().velocity;
-            currentChild.SetActive(true);
+            ShapeShift(Shift.Previous);
         }
+        else if (Input.GetKeyDown(KeyCode.RightShift))
+        {
+            ShapeShift(Shift.Next);
+        }
+    }
+
+    private void ShapeShift(Shift direction)
+    {
+        GameObject previousChild = currentChild;
+        Vector3 newPosition = previousChild.transform.position;
+        previousChild.SetActive(false);
+        currentChild = (direction == Shift.Previous) ? GetPreviousChild() : GetNextChild();
+        currentChild.transform.position = newPosition;
+        currentChild.GetComponent<Rigidbody>().velocity = previousChild.GetComponent<Rigidbody>().velocity;
+        currentChild.SetActive(true);
+    }
+
+    private GameObject GetNextChild()
+    {
+        childIndex++;
+        if (childIndex >= children.Length)
+        {
+            childIndex = 0;
+        }
+        return children[childIndex];
+    }
+
+    private GameObject GetPreviousChild()
+    {
+        childIndex--;
+        if (childIndex < 0)
+        {
+            childIndex = children.Length - 1;
+        }
+        return children[childIndex];
     }
 }
