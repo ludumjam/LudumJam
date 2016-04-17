17af20d48dbf26bd723b7aa7d96e871e804305c2
Kasper Lind Sørensen <kasper.lind.sorensen@gmail.com>
2016-04-17 21:43:29 +0200

    Leaderboard entries now have their recttransform scale set to 1 instead of 1.5

--- Assets/Code/LeaderBoardUI.cs
+++ Assets/Code/LeaderBoardUI.cs
@@ -63,7 +63,7 @@ public class LeaderBoardUI : MonoBehaviour
                     scoreObject.transform.SetParent(leaderboardPanel.transform);
                     scoreObject.transform.localPosition = Vector3.zero;
                     RectTransform scoreRectTransform = scoreObject.GetComponent<RectTransform>();
-                    //scoreRectTransform.localScale = Vector3.one;
+                    scoreRectTransform.localScale = Vector3.one;
                     scoreRectTransform.localPosition = new Vector3(0, previousYPosition - offset, 0);
                     previousYPosition -= offset;
                     //scoreRectTransform.offsetMin = new Vector2(0, scoreRectTransform.offsetMin.y);
