4aecb11c4681f82a6e676190b1df1384ed6bdeea
Nils <lolnils@gmail.com>
2016-04-18 17:53:43 +0200

    changed center of mass for rektangle

---
+++ Assets/Code/centerOfMass.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class centerOfMass : MonoBehaviour {
+	public Vector2 offset;
+	public Rigidbody2D rb;
+	void Start() {
+		rb = GetComponent<Rigidbody2D>();
+		rb.centerOfMass = offset;
+	}
+}
